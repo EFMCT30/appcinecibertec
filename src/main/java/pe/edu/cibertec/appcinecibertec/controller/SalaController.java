89778f90df7371a42f464eb631559ff202e4cf1d
EFMCT30 <enzofranchesco.ec@gmail.com>
2023-05-21 11:44:22 -0500

    Actu

--- src/main/java/pe/edu/cibertec/appcinecibertec/controller/SalaController.java
+++ src/main/java/pe/edu/cibertec/appcinecibertec/controller/SalaController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import pe.edu.cibertec.appcinecibertec.model.bd.Estado;
+import pe.edu.cibertec.appcinecibertec.model.bd.Sala;
 import pe.edu.cibertec.appcinecibertec.model.request.SalaRequest;
 import pe.edu.cibertec.appcinecibertec.model.response.ResutadoResponse;
 import pe.edu.cibertec.appcinecibertec.service.SalaService;
@@ -29,6 +31,23 @@ public class SalaController {
 	@PostMapping("/registrarSala")
 	@ResponseBody
 	public ResutadoResponse registrarSala(@RequestBody SalaRequest salaRequest) {
-		return null;
+		String mensaje="Sala Registrada Correctamente";
+		Boolean respuesta=true;
+		try {
+			Sala objSala = new Sala();
+			if(salaRequest.getIdsala() > 0) {
+				objSala.setIdsala(salaRequest.getIdsala());
+			}
+			objSala.setDescsala(salaRequest.getDescsala());
+			objSala.setAsientos(salaRequest.getAsientos());
+			Estado objEstado = new Estado();
+			objEstado.setIdestado(salaRequest.getIdestado());
+			objSala.setEstado(objEstado);
+			salaService.registrarSala(objSala);
+		} catch (Exception ex) {
+			mensaje="sala no registrada";
+			respuesta=false;
+		}
+		return ResutadoResponse.builder().mensaje(mensaje).respuesta(respuesta).build();
 	}
 }
